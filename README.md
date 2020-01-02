2eb6a5b18e3290c1cb77d58b6b457fcb5da42b83
Pauan <pauanyu+github@pm.me>
2020-01-02 03:33:06 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# rollup-plugin-rust
-Rollup plugin for bundling or importing Rust crates
+# rust-plugin-rust
+
+> Rollup plugin for bundling or importing Rust crates
+
+## Installation
+
+```sh
+yarn add --dev wasm-pack
+yarn add --dev github:Pauan/rollup-plugin-rust
+```
+
+### `wasm-pack`
+
+This plugin internally uses [`wasm-pack`](https://rustwasm.github.io/wasm-pack/) and [`wasm-bindgen`](https://rustwasm.github.io/docs/wasm-bindgen/), which is why you must install the `wasm-pack` npm package in order for it to work.
+
+## Usage
+
+Add the plugin in your `rollup.config.js` and import a `Cargo.toml` file:
+
+```js
+import rust from "rollup-plugin-rust";
+
+export default {
+    input: {
+        foo: "Cargo.toml",
+    },
+    plugins: [
+        rust(),
+    ],
+};
+```
+
+You can import as many different `Cargo.toml` files as you want, each one will be compiled separately.
+
+When compiling multiple crates it is recommended to use a single shared [workspace](https://doc.rust-lang.org/cargo/reference/manifest.html#the-workspace-section) to improve compile times.
---
+++ index.js
@@ -0,0 +1,242 @@
+const $fs = require("fs");
+const $glob = require("glob");
+const $path = require("path");
+const $child = require("child_process");
+const $toml = require("toml");
+const $rimraf = require("rimraf");
+const { createFilter } = require("rollup-pluginutils");
+
+
+function glob(pattern, cwd) {
+    return new Promise(function (resolve, reject) {
+        $glob(pattern, {
+            cwd: cwd,
+            strict: true,
+            absolute: true,
+            nodir: true
+        }, function (err, files) {
+            if (err) {
+                reject(err);
+
+            } else {
+                resolve(files);
+            }
+        });
+    });
+}
+
+function rm(path) {
+    return new Promise(function (resolve, reject) {
+        $rimraf(path, { glob: false }, function (err) {
+            if (err) {
+                reject(err);
+
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+function read(path) {
+    return new Promise(function (resolve, reject) {
+        $fs.readFile(path, function (err, file) {
+            if (err) {
+                reject(err);
+
+            } else {
+                resolve(file);
+            }
+        });
+    });
+}
+
+function wait(p) {
+    return new Promise((resolve, reject) => {
+        p.on("close", (code) => {
+            if (code === 0) {
+                resolve();
+
+            } else {
+                reject(new Error("Command `" + p.spawnargs.join(" ") + "` failed with error code: " + code));
+            }
+        });
+
+        p.on("error", reject);
+    });
+}
+
+
+const state = {
+    locked: false,
+    pending: [],
+};
+
+async function lock(f) {
+    if (state.locked) {
+        await new Promise(function (resolve, reject) {
+            state.pending.push(resolve);
+        });
+
+        if (state.locked) {
+            throw new Error("Invalid lock state");
+        }
+    }
+
+    state.locked = true;
+
+    try {
+        return await f();
+
+    } finally {
+        state.locked = false;
+
+        if (state.pending.length !== 0) {
+            const resolve = state.pending.shift();
+            // Wake up pending task
+            resolve();
+        }
+    }
+}
+
+
+async function wasm_pack(cx, dir, source, id, options) {
+    const toml = $toml.parse(source);
+
+    const name = toml.package.name;
+
+    // TODO use some logic to find the target dir
+    const out_dir = $path.resolve($path.join("target", "wasm-pack", name));
+
+    await rm(out_dir);
+
+    const args = [
+        // TODO adjust based on Webpack's error report level
+        //"--log-level", "error",
+        "build",
+        "--out-dir", out_dir,
+        "--out-name", "index",
+        "--target", "web",
+        "--no-typescript",
+        (options.debug ? "--dev" : "--release")
+    ];
+
+    try {
+        // TODO what if it tries to build the same crate multiple times ?
+        await lock(async function () {
+            // TODO make sure to use the npm installed binary for wasm-pack
+            await wait($child.spawn("wasm-pack", args, { cwd: dir, stdio: "inherit" }));
+        });
+
+    // TODO print the full error in verbose mode
+    } catch (e) {
+        throw new Error("");
+    }
+
+    const wasm = await read($path.join(out_dir, "index_bg.wasm"));
+
+    // TODO use the [name] somehow
+    // TODO generate random name ?
+    const wasm_name = name + ".wasm";
+
+    cx.emitFile({
+        type: "asset",
+        source: wasm,
+        fileName: wasm_name
+    });
+
+    // TODO better way to generate the path
+    const import_path = JSON.stringify("./" + $path.relative(dir, $path.join(out_dir, "index.js")));
+
+    const import_wasm = (options.importHook ? options.importHook(wasm_name) : JSON.stringify(wasm_name));
+
+    const is_entry = cx.getModuleInfo(id).isEntry;
+
+    if (is_entry) {
+        return {
+            code: `
+                import init from ${import_path};
+
+                init(${import_wasm}).catch(console.error);
+            `,
+            map: { mappings: '' }
+        };
+
+    // TODO this is probably wrong
+    } else {
+        return {
+            code: `
+                import init from ${import_path};
+
+                export default init(${import_wasm});
+            `,
+            map: { mappings: '' }
+        };
+    }
+}
+
+
+async function watch_files(cx, dir, options) {
+    if (options.watch) {
+        const matches = await await Promise.all(options.watchPatterns.map(function (pattern) {
+            return glob(pattern, dir);
+        }));
+
+        // TODO deduplicate matches ?
+        matches.forEach(function (files) {
+            files.forEach(function (file) {
+                cx.addWatchFile(file);
+            });
+        });
+    }
+}
+
+
+async function build(cx, source, id, options) {
+    const dir = $path.dirname(id);
+
+    const [output] = await Promise.all([
+        wasm_pack(cx, dir, source, id, options),
+        watch_files(cx, dir, options),
+    ]);
+
+    return output;
+}
+
+
+module.exports = function rust(options = {}) {
+    // TODO should the filter affect the watching ?
+    // TODO should the filter affect the Rust compilation ?
+    const filter = createFilter(options.include, options.exclude);
+
+    if (options.watchPatterns == null) {
+        options.watchPatterns = [
+            "src/**"
+        ];
+    }
+
+    return {
+        name: "rust",
+
+        buildStart(rollup) {
+            if (rollup.watch) {
+                if (options.watch == null) {
+                    options.watch = true;
+                }
+
+                if (options.debug == null) {
+                    options.debug = true;
+                }
+            }
+        },
+
+        transform(source, id) {
+            if ($path.basename(id) === "Cargo.toml" && filter(id)) {
+                return build(this, source, id, options);
+
+            } else {
+                return null;
+            }
+        },
+    };
+};
